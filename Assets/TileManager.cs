8360cb476392dfe26ec308b442d9dd22b79bd892
Patrick Quach <patrickquach96@gmail.com>
2019-02-16 14:46:09 -0800

    Added TileManager object to hold new generated tiles, TileManager script to dynamically spawns tile based on player current position

---
+++ Assets/TileManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tiles;
+    private Transform player;
+    private int spawnPos = 0;
+    private int tilesOnScreen = 5;
+    private int tileLength = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+
+        for(int i = 0; i < tilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.z > (spawnPos - tileLength * tilesOnScreen))
+        {
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile()
+    {
+        GameObject obj = GameObject.Instantiate(tiles[0]);
+        obj.transform.position = new Vector3(0, 0, spawnPos);
+        //set newly spawned tile to be TileManager children
+        obj.transform.SetParent(transform);
+        spawnPos += tileLength;
+
+    }
+}
