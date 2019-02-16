d0f8f89e32c6a127fc7171898012920c6e7dd3a8
Patrick Quach <patrickquach96@gmail.com>
2019-02-15 22:42:21 -0800

    Importing Tiger model from Unity Store as player character

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController controller;
+    private float speed = 2f;
+    private Vector3 movement;
+    private float verticalVelocity = 0.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = Vector3.zero;
+
+        movement.x = Input.GetAxisRaw("Horizontal") * speed;
+        movement.y = verticalVelocity;
+        movement.z = 1 * speed;
+        controller.Move(movement * Time.deltaTime);
         
     }
 }
