2141c101ed05c84c5716961bed2a131889f013c2
Patrick Quach <patrickquach96@gmail.com>
2019-02-17 17:42:40 -0800

    Improved ObjectPooler to store multiple different GameObject types using Dictionary, Key: prefab tag, Value: List of prefab

---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    //Set the configuration to be visible in the Unity inspector
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, List<GameObject>> poolDictionary;
+    private GameObject currentPrefab;
+
+    //Singleton: the instance of this script
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, List<GameObject>>();
+
+        //Initialize all different type of GameObject pools
+        foreach(Pool pool in pools)
+        {
+            List<GameObject> objectPool = new List<GameObject>();
+            for(int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Add(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    //return a reference to a prefab in the pool based on tag name
+    public GameObject GetPooledObject(string tag)
+    {
+        if(!poolDictionary.ContainsKey(tag))
+        {
+            Debug.Log("Pool with tag " + tag + " not found!");
+            return null;
+        }
+
+        for (int i = 0; i < poolDictionary[tag].Count; i++)
+        {
+            if (!poolDictionary[tag][i].activeInHierarchy)
+            {
+                poolDictionary[tag][i].SetActive(true);
+                return poolDictionary[tag][i];
+            }
+        }
+
+        //if no reusable object in pool, create new one
+        currentPrefab = getCurrentPrefab(tag);
+        GameObject obj = Instantiate(currentPrefab) as GameObject;
+        poolDictionary[tag].Add(obj);
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void ReturnPooledObject(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    public GameObject getCurrentPrefab(string tag)
+    {
+        for (int i = 0; i < pools.Count; i++)
+        {
+            if (pools[i].tag == tag)
+            {
+                return pools[i].prefab;
+            }
+        }
+
+        return null;
+    }
+}
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    public GameObject[] tiles;
+    //public GameObject[] tiles;
     private Transform player;
     private int tilesOnScreen = 10;
-    private int tileLength = 4;
-    private List<GameObject> activeTiles;
-    private int count;
+    private int tileLength = 5;
     private float speed = 3.0f;
+    private List<GameObject> activeTiles;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -19,14 +19,18 @@ public class TileManager : MonoBehaviour
         int spawnPos = 0;
         for (int i = 0; i < tilesOnScreen; i++)
         {
-            GameObject obj = ObjectPool.current.GetPooledObject();
-            obj.SetActive(true);
+            GameObject obj = ObjectPooler.Instance.GetPooledObject("Tile");
             obj.transform.position = new Vector3(0, 0, spawnPos);
             //set newly spawned tile to be ObjectPool children
-            obj.transform.SetParent(ObjectPool.current.transform);
+            obj.transform.SetParent(ObjectPooler.Instance.transform);
             spawnPos += tileLength;
             activeTiles.Add(obj);
         }
+
+    }
+
+    public void moveTiles(float s)
+    {
 
     }
 
@@ -37,7 +41,7 @@ public class TileManager : MonoBehaviour
             activeTiles[i].transform.Translate(-Vector3.forward * Time.deltaTime * speed);
         }
 
-        if(player.position.z > activeTiles[2].transform.position.z)
+        if (player.position.z > activeTiles[2].transform.position.z)
         {
             SpawnTile();
             DestroyTile();
@@ -54,19 +58,18 @@ public class TileManager : MonoBehaviour
 
     private void SpawnTile()
     {
-        GameObject obj = ObjectPool.current.GetPooledObject();
-        obj.SetActive(true);
+        GameObject obj = ObjectPooler.Instance.GetPooledObject("Tile");
         //set position of new tile to be at the last tile in the activeTiles[] list
         obj.transform.position = new Vector3(0, 0, activeTiles[tilesOnScreen-1].transform.position.z + tileLength);
         //set newly spawned tile to be ObjectPool children
-        obj.transform.SetParent(ObjectPool.current.transform);
+        obj.transform.SetParent(ObjectPooler.Instance.transform);
         activeTiles.Add(obj);
     }
 
     private void DestroyTile()
     {
         //put back "destroyed" object into ObjectPool
-        activeTiles[0].SetActive(false);
+        ObjectPooler.Instance.ReturnPooledObject(activeTiles[0]);
         activeTiles.RemoveAt(0);
     }
 }
