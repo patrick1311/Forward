b376f874d477f85b7a879d13afa9da4d0b7fb6e1
Patrick Quach <patrickquach96@gmail.com>
2019-02-25 22:52:27 -0800

    Add function to check for player movement safety. Specifically to prevent player from clipping into the side collider of the Tile prefab.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
     private Rigidbody rb;
+    private MeshCollider mesh;
     private float speed = 4.8f;
     private Vector3 movement;
     private bool movementEnabled = true;
@@ -21,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
+        mesh = GetComponent<MeshCollider>();
+        Debug.Log(mesh.bounds.extents.x);
     }
 
     // Update is called once per frame
@@ -34,7 +37,6 @@ public class PlayerMovement : MonoBehaviour
 
             if(Input.touchCount > 0)
             {
-                //Vector2 pos = Input.GetTouch(0).position;
                 if (Input.GetTouch(0).position.x > width)
                 {
                     movement.x = speed;
@@ -45,15 +47,31 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
 
-            transform.Translate(movement * Time.deltaTime);
+            //Prevent player clipping into other objects
+            if(SafeToMove(movement))
+                transform.Translate(movement * Time.deltaTime);
         }
 
+    }
+
+    bool SafeToMove(Vector3 translation)
+    {
+        //check whether there is collider at the position player about to move to
+        Collider[] cols = Physics.OverlapSphere(transform.position + translation * Time.deltaTime, mesh.bounds.extents.x);
+        foreach (Collider col in cols)
+        {
+            if (col.tag == "SideBarrier")
+            {
+                return false;
+            }
+        }
+        return true;      
     }
 
     public void SetPlayerSpeed(float gameSpeed)
     {
         speed = gameSpeed * 0.6f;
-        Debug.Log("Player speed: " + speed);
+        //Debug.Log("Player speed: " + speed);
     }
 
     public void StopMovement()
